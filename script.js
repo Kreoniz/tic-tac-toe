05ef13d3229459589f5b4cf6783a81007de5c9e5
Kreoniz <kreoniz@mail.ru>
2023-06-08 19:01:20 +0600

    Add functionality for placing marks

--- script.js
+++ script.js
@@ -2,26 +2,38 @@ const root = document.querySelector("#board")
 
 const Gameboard = (function() {
     const gameboard = [
-        ["X", "O", " "],
-        ["O", " ", "X"],
+        ["X", "O", ""],
+        ["O", "", "X"],
         ["O", "X", "O"],
     ];
+
+    function addMark(cell, player) {
+        if (!cell.textContent) {
+            cell.textContent = player;
+        }
+    }
 
     function render(board) {
         for (let row = 0; row < gameboard.length; row++) {
             const boardRow = document.createElement("div");
 
             for (let column = 0; column < gameboard[row].length; column++) {
-                const field = document.createElement("div");
-                field.textContent = gameboard[row][column];
+                const boardCell = document.createElement("div");
 
-                boardRow.appendChild(field);
+                boardCell.textContent = gameboard[row][column];
+                boardCell.classList.add("cell"); 
+
+                const cellIndex = `${row}${column}`;
+                boardCell.dataset.index = cellIndex;
+
+                boardCell.addEventListener("click", () => addMark(boardCell, "X"))
+
+                boardRow.appendChild(boardCell);
             }
             boardRow.classList.add("row");
             board.appendChild(boardRow);
         }
     }
-
     return {
         render
     }
