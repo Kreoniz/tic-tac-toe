b1a6971db8f2545758ae4957245e697885a6b6a5
Kreoniz <kreoniz@mail.ru>
2023-06-09 13:19:52 +0600

    Rework and allow player to place marks

--- script.js
+++ script.js
@@ -1,42 +1,98 @@
-const root = document.querySelector("#board")
-
 const Gameboard = (function() {
-    const gameboard = [
-        ["X", "O", ""],
-        ["O", "", "X"],
-        ["O", "X", "O"],
-    ];
+    const rows = 3;
+    const columns = 3;
 
-    function addMark(cell, player) {
-        if (!cell.textContent) {
-            cell.textContent = player;
+    const gameboard = [];
+    const getBoard = () => gameboard;
+
+    for (let i = 0; i < rows; i++) {
+        gameboard[i] = [];
+        for (let j = 0; j < columns; j++) {
+            gameboard[i].push("");
         }
     }
 
-    function render(board) {
+    function setMark(row, column, mark) {
+        if (!gameboard[row][column]) {
+            gameboard[row][column] = mark;
+        }
+    }
+
+    return {
+        getBoard,
+        setMark,
+    }
+})();
+
+const Player = function(mark) {
+    return {
+        mark,
+    };
+}
+
+const GameController = (function() {
+    const players = {
+        first: Player("X"),
+        second: Player("O"),
+    }
+    const board = Gameboard.getBoard();
+
+    let activePlayer = players.first;
+
+    const switchPlayers = () => activePlayer = activePlayer === players.first ? players.second : players.first;
+
+    function playMove(row, column) {
+        if (!board[row][column]) {
+            board[row][column] = activePlayer.mark;
+            switchPlayers();
+        }
+
+        return board;
+    }
+
+    return {
+        playMove,
+    }
+})();
+
+function ScreenController() {
+    const root = document.querySelector("#board")
+
+    let gameboard = Gameboard.getBoard();
+
+    function renderBoard(root) {
+        root.textContent = "";
+
         for (let row = 0; row < gameboard.length; row++) {
             const boardRow = document.createElement("div");
 
             for (let column = 0; column < gameboard[row].length; column++) {
-                const boardCell = document.createElement("div");
+                const boardCell = document.createElement("button");
 
                 boardCell.textContent = gameboard[row][column];
                 boardCell.classList.add("cell"); 
 
-                const cellIndex = `${row}${column}`;
-                boardCell.dataset.index = cellIndex;
-
-                boardCell.addEventListener("click", () => addMark(boardCell, "X"))
+                boardCell.dataset.index = [row, column];
 
                 boardRow.appendChild(boardCell);
             }
             boardRow.classList.add("row");
-            board.appendChild(boardRow);
+            root.appendChild(boardRow);
         }
     }
-    return {
-        render
-    }
-})();
 
-Gameboard.render(root);
+    function clickHandlerBoard(e) {
+        const index = e.target.dataset.index;
+        const [row, column] = index.split(",");
+
+        gameboard = GameController.playMove(row, column);
+        console.log(gameboard);
+        renderBoard(root);
+    }
+
+    renderBoard(root);
+
+    root.addEventListener("click", clickHandlerBoard);
+}
+
+ScreenController();
